3ee26c2e076c8caa9fee28fbe1b5026e9be86bb0
Heikki Linnakangas <hlinnakangas@pivotal.io>
2017-09-29 10:42:27 +0300

    Move checking an explicit VARIADIC "any" argument into the parser.
    Cherry-pick of:
    
    commit d26888bc4d1e539a82f21382b0000fe5bbf889d9
    Author: Andrew Dunstan <andrew@dunslane.net>
    Date:   Thu Jul 18 11:52:12 2013 -0400
    
        Move checking an explicit VARIADIC "any" argument into the parser.
    
        This is more efficient and simpler . It does mean that an untyped NULL
        can no longer be used in such cases, which should be mentioned in
        Release Notes, but doesn't seem a terrible loss. The workaround is to
        cast the NULL to some array type.
    
        Pavel Stehule, reviewed by Jeevan Chalke.

--- src/backend/catalog/pg_aggregate.c
+++ src/backend/catalog/pg_aggregate.c
@@ -390,6 +390,7 @@ lookup_agg_function(List *fnName,
 	Oid			fnOid;
 	bool		retset;
 	int			nvargs;
+	Oid			vatype;
 	Oid		   *true_oid_array;
 	FuncDetailCode fdresult;
 	AclResult	aclresult;
@@ -404,7 +405,8 @@ lookup_agg_function(List *fnName,
 	 */
 	fdresult = func_get_detail(fnName, NIL, nargs, input_types, false, false,
 							   &fnOid, rettype, &retset,
-							   &nvargs, &true_oid_array, NULL);
+							   &nvargs, &vatype,
+							   &true_oid_array, NULL);
 
 	/* only valid case is a normal function not returning a set */
 	if (fdresult != FUNCDETAIL_NORMAL || !OidIsValid(fnOid))
